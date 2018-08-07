350a5e360e3280375bb64bea3b7c08c149b0b5d1
paulr_gfl12 <paulriabov@gmail.com>
2018-08-07 16:35:24 +0300

    dev task1

---
+++ tmp/app/views/contactUs.php
@@ -0,0 +1,28 @@
+<html>
+<head></head>
+<body>
+  <h1>Contact us</h1>  
+    <form action="" method="post">
+<div>
+
+        <p>
+            <label for="name">name</label>
+        </p>
+        <p>
+            <input type="text" name="name" id="name" value="" />
+        </p>
+</div>
+<div>
+        <p>
+            <label for="message">message</label>
+        </p>
+        <p>
+            <textarea name="message" id="message" rows="8" cols="40"></textarea>
+        </p>
+
+</div>
+        <button type="submit">Send</button>
+    </form>
+</body>
+
+</html>
---
+++ tmp/app/views/thankYou.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+  <h1>Thank you!</h1>  
+    <div>
+        Thank You, $name!
+    </div>
+</body>
+
+</html>
